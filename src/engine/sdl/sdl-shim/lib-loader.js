e7d4aa0fff17771e2f6666a248bef7f3c2dbca7e
Marlon Chatman <mcchatman8009@gmail.com>
2018-12-09 00:14:15 -0600

    Update the libraries and add windows support

--- src/engine/sdl/sdl-shim/lib-loader.js
+++ src/engine/sdl/sdl-shim/lib-loader.js
@@ -4,6 +4,8 @@ function getLibraryPath() {
     switch (process.platform) {
         case 'darwin':
             return getOSXLibraryPath();
+        case 'win32':
+            return getWindowsLibraryPath();
         case 'linux':
             return getLinuxLibraryPath();
         default:
@@ -11,21 +13,32 @@ function getLibraryPath() {
     }
 }
 
+function getWindowsLibraryPath() {
+    const dir = __dirname;
+    const arch = process.arch;
+
+    return `${dir}/libs/windows/${arch}`;
+}
+
 function getOSXLibraryPath() {
     const dir = __dirname;
+    const arch = process.arch;
 
-    return `${dir}/libs/osx`;
+    return `${dir}/libs/osx/${arch}`;
 }
 
 function getLinuxLibraryPath() {
     const dir = __dirname;
+    const arch = process.arch;
 
-    return `${dir}/libs/linux`;
+    return `${dir}/libs/linux/${arch}`;
 }
 
 function load(library, module, moduleExports) {
     if (library === 'libSDL2') {
-        library = 'libSDL2-2.0'
+        library = 'libSDL2-2.0';
+    } else {
+        library = 'SDL2-2.0';
     }
     const path = getLibraryPath();
     const moduleLibrary = `${path}/${library}`;
